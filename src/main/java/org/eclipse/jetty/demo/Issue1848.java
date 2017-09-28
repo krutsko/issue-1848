386fc3c8ce9f457196d403c342f7bcacb0d741db
Sergey Krutsko <skrutsko@odesk.com>
2017-09-28 11:46:32 +0300

    make payload size configurable

--- src/main/java/org/eclipse/jetty/demo/Issue1848.java
+++ src/main/java/org/eclipse/jetty/demo/Issue1848.java
@@ -1,15 +1,5 @@
 package org.eclipse.jetty.demo;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.concurrent.Executors;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.eclipse.jetty.io.ArrayByteBufferPool;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Request;
@@ -19,6 +9,15 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Issue1848
 {
@@ -47,6 +46,14 @@ public class Issue1848
             LOG.info("Not setting connector.setSoLingerTime()");
         }
 
+        int payloadSize = 5_000_000;
+        if (args.length == 2)
+        {
+            payloadSize = Integer.parseInt(args[1]);
+        }
+        LOG.info("Using response payload size={} bytes", payloadSize);
+        byte[] chars = new byte[payloadSize];
+
         connector.setName("default");
         server.addConnector(connector);
         server.setHandler(new AbstractHandler()
@@ -56,7 +63,6 @@ public class Issue1848
             {
                 response.setContentType("application/octet-stream");
                 response.setStatus(HttpServletResponse.SC_OK);
-                byte[] chars = new byte[5_000_000];
                 Arrays.fill(chars, (byte) 'x');
                 response.getOutputStream().write("Start of content...\n".getBytes(UTF_8));
                 response.getOutputStream().write(chars);
